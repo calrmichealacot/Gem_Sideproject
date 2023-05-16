df286e1d1f16db2446f324d14cfa72853bcd3bce
Calr Micheal Acot <calrmichealacot@gmail.com>
2023-05-16 16:30:49 +0800

    adding view and controller to admin/user

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,27 +1,31 @@
 # frozen_string_literal: true
 
 class Admin::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def create
+    @user = User.find_by_email(params[:admin_user][:email])
+    if @user&.admin? && @user&.valid_password?(params[:admin_user][:password])
+      flash[:alert] = "You're not authorized to login"
+      redirect_to new_admin_users_session_path
+    else
+      super
+    end
+  end  # DELETE /resource/sign_out
+  def destroy
+    super
+  end
 
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  end
 end
