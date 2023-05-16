d3329f53aabc129544325fd00ae7f04a300c13b6
Calr Micheal Acot <calrmichealacot@gmail.com>
2023-05-16 16:29:04 +0800

    [Feature 1] add column to Phone_number and add phoneLib

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum genre: { client: 0, admin: 1 }
+  validates :phone_number, phone: {
+    possible: true,
+    allow_blank: true,
+    types: %i[voip mobile],
+    countries: [:ph]
+  }
 end
