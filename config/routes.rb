4210df7724b279eccf7bf75e090f690b7d82806e
Calr Micheal Acot <calrmichealacot@gmail.com>
2023-05-16 15:47:19 +0800

    [Feature 2] adding Route Constraint

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'home#index'
+  constraints(ClientDomainConstraint.new) do
+    resources :home do
+
+    end
+  end
+ constraints(AdminDomainConstraint.new) do
+     namespace :admin do
+       resources :users
+     end
+ end
 end
